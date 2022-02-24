9d6e16efad2c2c41fb90503e2acc8190e4d265c2
rochafialvin <rochafi.dev@gmail.com>
2022-02-24 11:32:41 +0700

    Create route for create txt file

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+const fs = require("fs").promises;
 const app = express();
 const port = 2022;
 
 app.use(cors());
+app.use(express.json());
+app.use("/public", express.static("public"));
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello World");
@@ -12,6 +15,30 @@ app.get("/", (req, res) => {
 app.get("/console", (req, res) => {
   console.log("Route console telah diakses");
   res.send("Lihat terminal");
+});
+
+app.post("/files", async (req, res, next) => {
+  try {
+    const { title, body } = req.body;
+    const fileName = `${title.toLowerCase()}.txt`;
+    const filePath = `./public/${fileName}`;
+
+    await fs.writeFile(filePath, body);
+
+    res.send({
+      status: "SUCCESS",
+      message: `New file : ${fileName}`,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.use((err, req, res, next) => {
+  res.send({
+    status: "ERROR",
+    message: err.message,
+  });
 });
 
 app.listen(port, () => console.log(`Sever is running at ${port}`));
